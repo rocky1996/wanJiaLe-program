451272828a7faf98c953af1945f0c84ffb9b7dd0
rocky1996 <2791752775@qq.com>
2022-09-05 11:16:45 +0800

    add 管理员日志表 table

---
+++ src/main/java/com/wanjiale/wanjialeprogram/domain/FaAdminLog.java
@@ -0,0 +1,71 @@
+package com.wanjiale.wanjialeprogram.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fa_admin_log")
+public class FaAdminLog {
+
+    /**
+     * ID
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 管理员ID
+     */
+    @Column(name = "admin_id")
+    private Integer adminId;
+
+    /**
+     * 管理员名字
+     */
+    @Column(name = "username")
+    private String username;
+
+    /**
+     * 操作页面
+     */
+    @Column(name = "url")
+    private String url;
+
+    /**
+     * 日志标题
+     */
+    @Column(name = "title")
+    private String title;
+
+    /**
+     * 内容
+     */
+    @Column(name = "content")
+    private String content;
+
+    /**
+     * IP
+     */
+    @Column(name = "ip")
+    private String ip;
+
+    /**
+     * User-Agent
+     */
+    @Column(name = "useragent")
+    private String userAgent;
+
+    /**
+     * 操作时间
+     */
+    @Column(name = "createtime")
+    private String createTime;
+}
