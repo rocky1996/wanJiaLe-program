bd82bded8f6c7dc00104ab2cf96337a0fe2afffd
rocky1996 <2791752775@qq.com>
2022-09-05 11:36:01 +0800

    add 分组表 table

---
+++ src/main/java/com/wanjiale/wanjialeprogram/domain/FaAuthGroup.java
@@ -0,0 +1,59 @@
+package com.wanjiale.wanjialeprogram.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fa_auth_group")
+public class FaAuthGroup {
+
+    /**
+     * ID
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 父组别
+     */
+    @Column(name = "pid")
+    private Integer pid;
+
+    /**
+     * 组名
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * 规则ID
+     */
+    @Column(name = "rules")
+    private String rules;
+
+    /**
+     * 更新时间
+     */
+    @Column(name = "updatetime")
+    private Long updateTime;
+
+    /**
+     * 上传时间
+     */
+    @Column(name = "uploadtime")
+    private Long uploadTime;
+
+    /**
+     * 状态
+     */
+    @Column(name = "status")
+    private String status;
+}
