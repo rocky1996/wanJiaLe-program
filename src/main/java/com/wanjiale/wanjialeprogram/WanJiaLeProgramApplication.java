b3021a94a43b3143977ea0c1e9d1760ca46ef583
rocky1996 <2791752775@qq.com>
2022-09-05 10:20:02 +0800

    初始化项目

---
+++ src/main/java/com/wanjiale/wanjialeprogram/WanJiaLeProgramApplication.java
@@ -0,0 +1,13 @@
+package com.wanjiale.wanjialeprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanJiaLeProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanJiaLeProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanjiale/wanjialeprogram/WanJiaLeProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanjiale.wanjialeprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanJiaLeProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
