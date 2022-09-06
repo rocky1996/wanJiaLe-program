1673b9579199159b08566cb29191e658b85b9e1e
rocky1996 <2791752775@qq.com>
2022-09-06 12:04:33 +0800

    add 会员Token表 table

---
+++ src/main/java/com/wanjiale/wanjialeprogram/domain/FaUserToken.java
@@ -0,0 +1,47 @@
+package com.wanjiale.wanjialeprogram.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fa_user_token")
+public class FaUserToken {
+
+    /**
+     * id
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * Token
+     */
+    @Column(name = "token")
+    private String token;
+
+    /**
+     * 会员ID
+     */
+    @Column(name = "user_id")
+    private Integer userId;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "createtime")
+    private Long createTime;
+
+    /**
+     * 过期时间
+     */
+    @Column(name = "expiretime")
+    private Long expireTime;
+}
