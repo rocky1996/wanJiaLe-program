36e8abb9e7c394be55152ea6875d354d8953d509
rocky1996 <2791752775@qq.com>
2022-09-06 10:49:57 +0800

    add 产品类目 table

---
+++ src/main/java/com/wanjiale/wanjialeprogram/domain/FaSa.java
@@ -0,0 +1,65 @@
+package com.wanjiale.wanjialeprogram.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fa_sa")
+public class FaSa {
+
+    /**
+     * id
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 栏目名称
+     */
+    @Column(name = "name")
+    private String name;
+
+    /**
+     * 广告图
+     */
+    @Column(name = "image")
+    private String image;
+
+    /**
+     * 栏目描述
+     */
+    @Column(name = "des")
+    private String des;
+
+    /**
+     * 栏目图片
+     */
+    @Column(name = "avatar")
+    private String avatar;
+
+    /**
+     * 权重
+     */
+    @Column(name = "weigh")
+    private Integer weigh;
+
+    /**
+     * 产品推荐
+     */
+    @Column(name = "switched")
+    private Integer switched;
+
+    /**
+     * 显示
+     */
+    @Column(name = "status")
+    private String status;
+}
