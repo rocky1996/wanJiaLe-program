04e4b7196668a4c2b4661ed987f5cde53721d96a
rocky1996 <2791752775@qq.com>
2022-09-06 11:01:57 +0800

    add 短信验证码表 table

---
+++ src/main/java/com/wanjiale/wanjialeprogram/domain/FaSms.java
@@ -0,0 +1,60 @@
+package com.wanjiale.wanjialeprogram.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fa_sms")
+public class FaSms {
+
+    /**
+     * id
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 事件
+     */
+    @Column(name = "event")
+    private String event;
+
+    /**
+     * 手机号
+     */
+    @Column(name = "mobile")
+    private String mobile;
+
+    /**
+     * 验证码
+     */
+    @Column(name = "code")
+    private String code;
+
+    /**
+     * 验证次数
+     */
+    @Column(name = "times")
+    private Integer times;
+
+    /**
+     * IP
+     */
+    @Column(name = "ip")
+    private String ip;
+
+    /**
+     * 创建时间
+     */
+    @Column(name = "createtime")
+    private Long createTime;
+}
