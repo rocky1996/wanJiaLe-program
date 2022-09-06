5edbb139bf9ebd105c0c55b3f2fd39f460f9979f
rocky1996 <2791752775@qq.com>
2022-09-06 10:19:53 +0800

    add 评论点赞记录 table

---
+++ src/main/java/com/wanjiale/wanjialeprogram/domain/FaCommentsLike.java
@@ -0,0 +1,41 @@
+package com.wanjiale.wanjialeprogram.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "fa_comments_like")
+public class FaCommentsLike {
+
+    /**
+     * id
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * 用户id
+     */
+    @Column(name = "uid")
+    private Integer uid;
+
+    /**
+     * 评论id
+     */
+    @Column(name = "cid")
+    private Integer cid;
+
+    /**
+     * 日期
+     */
+    @Column(name = "createtime")
+    private Long createTime;
+}
