8e06d19dafa8bbb4b30cfe8d15b5eeefca1f296c
rocky1996 <2791752775@qq.com>
2022-09-29 11:07:08 +0800

    初始化启动类

--- src/main/java/com/wanjiale/wanjialeprogram/WanJiaLeProgramApplication.java
+++ src/main/java/com/wanjiale/wanjialeprogram/WanJiaLeProgramApplication.java
@@ -1,9 +1,16 @@
 package com.wanjiale.wanjialeprogram;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableAsync
+@MapperScan(basePackages = "com.wanjiale.wanjialeprogram.dao")
+@EnableJpaRepositories
 public class WanJiaLeProgramApplication {
 
 	public static void main(String[] args) {
