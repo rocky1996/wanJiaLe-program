b678ba394ab59e6cef514951e087c20f57c02c3c
rocky1996 <2791752775@qq.com>
2022-09-29 11:22:06 +0800

    test

--- src/main/java/com/wanjiale/wanjialeprogram/dao/FaApiLogDao.java
+++ src/main/java/com/wanjiale/wanjialeprogram/dao/FaApiLogDao.java
@@ -1,9 +1,19 @@
 package com.wanjiale.wanjialeprogram.dao;
 
 import com.wanjiale.wanjialeprogram.domain.FaApiLogEntity;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FaApiLogDao extends JpaRepository<FaApiLogEntity,Integer> {
+
+    @Query(value = "select en from FaApiLogEntity en")
+    List<FaApiLogEntity> selectAll();
+
+    @Query(value = "select en from FaApiLogEntity en where en.id = ?1")
+    FaApiLogEntity selectById(@Param("id") Integer id);
 }
