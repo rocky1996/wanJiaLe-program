656fdee08887511489347f510e5f9f47e8152075
rocky1996 <2791752775@qq.com>
2022-09-29 11:29:03 +0800

    test

--- src/main/java/com/wanjiale/wanjialeprogram/dao/FaAreaDao.java
+++ src/main/java/com/wanjiale/wanjialeprogram/dao/FaAreaDao.java
@@ -1,9 +1,19 @@
 package com.wanjiale.wanjialeprogram.dao;
 
 import com.wanjiale.wanjialeprogram.domain.FaAreaEntity;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FaAreaDao extends JpaRepository<FaAreaEntity,Integer> {
+
+    @Query(value = "select en from FaAreaEntity en")
+    List<FaAreaEntity> selectAll();
+
+    @Query(value = "select en from FaAreaEntity en where en.id = ?1")
+    FaAreaEntity selectById(@Param("id") Integer id);
 }
