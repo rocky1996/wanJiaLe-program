633e5c523bcfbe1d3196e240a1fd26f44a3ac249
rocky1996 <2791752775@qq.com>
2022-09-29 11:15:31 +0800

    test

--- src/main/java/com/wanjiale/wanjialeprogram/dao/FaAdminDao.java
+++ src/main/java/com/wanjiale/wanjialeprogram/dao/FaAdminDao.java
@@ -1,9 +1,19 @@
 package com.wanjiale.wanjialeprogram.dao;
 
 import com.wanjiale.wanjialeprogram.domain.FaAdminEntity;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FaAdminDao extends JpaRepository<FaAdminEntity,Integer> {
+
+    @Query(value = "select en from FaAdminEntity en")
+    List<FaAdminEntity> selectAll();
+
+    @Query(value = "select en from FaAdminEntity en where en.id = ?1")
+    FaAdminEntity selectById(@Param("id") Integer id);
 }
--- src/test/java/com/wanjiale/wanjialeprogram/WanJiaLeProgramApplicationTests.java
+++ src/test/java/com/wanjiale/wanjialeprogram/WanJiaLeProgramApplicationTests.java
@@ -1,13 +1,26 @@
 package com.wanjiale.wanjialeprogram;
 
+import com.wanjiale.wanjialeprogram.dao.FaAdminDao;
+import com.wanjiale.wanjialeprogram.domain.FaAdminEntity;
+import com.wanjiale.wanjialeprogram.util.JacksonUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
+@Slf4j
+@Component
 class WanJiaLeProgramApplicationTests {
+
+	@Resource
+	private FaAdminDao faAdminDao;
 
 	@Test
 	void contextLoads() {
+		FaAdminEntity faAdminEntity = faAdminDao.selectById(1);
+		log.info(JacksonUtil.beanToStr(faAdminEntity));
 	}
-
 }
